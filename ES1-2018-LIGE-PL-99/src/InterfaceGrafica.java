74108f5fc55bca1715dd295c63d05bf59a8710eb
jmgas1-iscteiul <jmgas1@iscte-iul.pt>
2018-10-23 22:38:44 +0100

    interface_login

--- ES1-2018-LIGE-PL-99/src/InterfaceGrafica.java
+++ ES1-2018-LIGE-PL-99/src/InterfaceGrafica.java
@@ -1,4 +1,86 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
 
-public class InterfaceGrafica {
-	//efgrhtejtr
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.DropMode;
+import javax.swing.JLabel;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import javax.swing.ImageIcon;
+import java.awt.SystemColor;
+
+public class InterfaceGrafica extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceGrafica frame = new InterfaceGrafica();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfaceGrafica() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 529, 336);
+		contentPane = new JPanel();
+		contentPane.setBackground(SystemColor.text);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(173, 0, 338, 289);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		textField = new JTextField();
+		textField.setForeground(Color.LIGHT_GRAY);
+		textField.setBounds(147, 87, 147, 32);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setForeground(Color.LIGHT_GRAY);
+		textField_1.setBounds(147, 160, 147, 32);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("User");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setToolTipText("");
+		lblNewLabel.setLabelFor(lblNewLabel);
+		lblNewLabel.setBounds(53, 90, 82, 26);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Password");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_1.setBounds(53, 160, 88, 26);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(InterfaceGrafica.class.getResource("/images/1413073433_images-2-.jpg")));
+		lblNewLabel_2.setLabelFor(lblNewLabel_2);
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(12, 68, 136, 134);
+		contentPane.add(lblNewLabel_2);
+	}
 }
