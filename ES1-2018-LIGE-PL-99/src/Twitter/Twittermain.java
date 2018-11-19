a7d5dbb76d20da0b2be764057ce8b63acbb12875
dmbjs-iscteiul <dmbju@iscte-iul.pt>
2018-11-19 14:02:57 +0000

    twitter feed

--- ES1-2018-LIGE-PL-99/src/Twitter/Twittermain.java
+++ ES1-2018-LIGE-PL-99/src/Twitter/Twittermain.java
@@ -1,9 +1,34 @@
 package Twitter;
 
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
 public class Twittermain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws TwitterException {
+		
+		ConfigurationBuilder cf = new ConfigurationBuilder();
+		
+		cf.setDebugEnabled(true)
+			.setOAuthConsumerKey("S3zvRA8J9QMlFFQneABHyNU68")
+			.setOAuthConsumerSecret("xYcjr59LMFWiWBTYPX4ZzZah8JAtAyaEPhZzeqrd4u1lZKcm9I")
+			.setOAuthAccessToken("561479088-UJZeNdtVGUbyyFCoNaaPqFkyOrYtPngufARZWeeb")
+			.setOAuthAccessTokenSecret("uSKkUOPxJSf9HyT76oLGEoY14g8mbDlp0tVSxCahBMRmz");
+		
+		TwitterFactory tf = new TwitterFactory(cf.build());
+		twitter4j.Twitter twitter=tf.getInstance();
+		
+		List<Status> Estado= twitter.getHomeTimeline();
+		
+		for(Status s : Estado) {
+			System.out.println(s.getUser().getName()+"  "+s.getText());
+		}
+		
+		
 
 	}
 
