0378e84f30101357ab78cf6f6d61a92c7f2f92f2
dmbjs-iscteiul <dmbju@iscte-iul.pt>
2018-12-03 17:12:43 +0000

    send mail gui parcial

--- es1-2018-99/src/main/java/main/java/GUI/SendMailGUI.java
+++ es1-2018-99/src/main/java/main/java/GUI/SendMailGUI.java
@@ -1,9 +1,53 @@
 package main.java.GUI;
 
-public class SendMailGUI {
+import java.awt.GridLayout;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+public class SendMailGUI {
+	
+	JFrame frame;
+	
+	private void addFrameContent() {
+		
+		JTextField destino = new JTextField("Destino");
+		JTextField assunto = new JTextField("Assunto");	
+		JTextField texto = new JTextField("texto");
+		JPanel info = new JPanel();
+		info.setLayout(new GridLayout(2,1));
+		JPanel corpo = new JPanel();
+		JPanel janela = new JPanel();
+		janela.setLayout(new GridLayout(2,1));
+		info.add(destino);
+		info.add(assunto);
+		
+		janela.add(info);
+		janela.add(texto);
+	
+		frame= new JFrame("Send Mail");
+		frame.add(janela);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame.setSize(800,800);
+		frame.setLocation(400,100);	
+		frame.setVisible(true);
+		
+	}
+	
+	void init() {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			public void run() {
+				addFrameContent();
+			}
+		});
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		SendMailGUI se = new SendMailGUI();
+		se.init();
 
 	}
 
